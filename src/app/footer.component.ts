185191fb30c7b878d7d603e02e30b58e4fc6051a
sandip.nirmal <sandip.nirmal@synerzip.com>
2017-01-31 10:48:41 +0530

    App structure

---
+++ src/app/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'athena-footer',
+  templateUrl: 'footer.template.html'
+})
+
+export class FooterComponent {}
---
+++ src/app/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'athena-header',
+  templateUrl: 'header.template.html'
+})
+
+export class HeaderComponent {}
