a0096e44ce4f1b880e30cbf725033e61fa6043eb
Vishakha Raut <vishakha.korade@synerzip.com>
2017-02-02 12:52:29 +0530

    Added eutils javascript library to make search.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -21,8 +21,21 @@ export class SearchComponent {
   public orgs = QueryOptions.orgs;
   public exps = QueryOptions.exp;
 
+  public search():any{
+  // It will return ncbi eutils object to make search.
+      return require("ncbi-eutils");
+  }
+
   // Need to get results from NCBI-DB and disaply it in tabular form
   getResult() : void {
+    var Eutils = this.search();
+    // Ex of Eutils search.
+    //    Eutils.esearch({db:'gene', term:'drugs', field:'vitamin A', retmax: 20, restart: 2})
+    //      .then(function(d){console.log(d)});
+    //    Eutils.esearch({db:'gene', term:'drugs', field:'vitamin A', retmax: 1, restart: 21})
+    //      .then(Eutils.esummary())
+    //      .then(function(d){console.log(d)});
+
     this.router.navigate(['/search-result']);
   }
 
