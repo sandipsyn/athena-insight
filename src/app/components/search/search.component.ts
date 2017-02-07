4116f8226c48d242cdf9235e61fb4c391cf0491c
sandip.nirmal <sandip.nirmal@synerzip.com>
2017-02-07 14:30:09 +0530

    Mocking eUtils data

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -58,28 +58,31 @@ export class SearchComponent {
 
     console.log('drug ' + this.drug + ' disease ' + this.disease + ' org ' + this.org + ' exp ' + this.exp);
 
-    Eutils.esearch({ db: 'gds', term: 'chordoma' })
-      .then((d) => {
-        //supported eutil parameters can be added like this
-        d.retstart = 5;
-        return Eutils.esummary(d);
-      })
-      .then((d) => {
-        console.log(d);
-        this.searchService.generateResult(d);
-        //this.searchService.generateResult(searchResult);
-        this.router.navigate(['/search-result']);
-      })
-      .catch((d) => {
-          console.log(d);
-          this.searchService.generateResult(searchResult);
-          this.router.navigate(['/search-result']);
-        } );
+    //console.time('eUtils Search');
 
-    //setTimeout(() => {
-    //  this.searchService.generateResult(searchResult);
-    //  this.router.navigate(['/search-result']);
-    //}, 1500);
+    //Eutils.esearch({ db: 'gds', term: 'chordoma' })
+    //  .then((d) => {
+    //    //supported eutil parameters can be added like this
+    //    d.retstart = 5;
+    //    return Eutils.esummary(d);
+    //  })
+    //  .then((d) => {
+    //      console.timeEnd('eUtils Search');
+    //
+    //      console.log(d);
+    //      this.searchService.generateResult(d);
+    //      this.router.navigate(['/search-result']);
+    //  })
+    //  .catch((d) => {
+    //      console.log(d);
+    //      this.searchService.generateResult(searchResult);
+    //      this.router.navigate(['/search-result']);
+    //    } );
+
+    setTimeout(() => {
+      this.searchService.generateResult(searchResult);
+      this.router.navigate(['/search-result']);
+    }, 2800);
 
   }
 
